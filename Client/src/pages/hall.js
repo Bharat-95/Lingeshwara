38b96573941264ce4ee68f15adb6874f263ba228
Bharat Kumar <bharat@Bharats-MacBook-Pro.local>
2023-10-14 23:49:27 +0530

    Added products from mysql

--- Client/src/pages/hall.js
+++ Client/src/pages/hall.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import "../app/globals.css";
 
 const hall = () => {
-  return (
-    <div>hall</div>
-  )
-}
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+      
+     fetch('http://localhost:4000/products')
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    }, []);
+  
+    if (data) {
+      
+      return (
+        <div>
+        <div className='text-2xl '>Products</div>
+        <div>
+        <ul className='grid grid-cols-2 grid-rows-2 gap-x-10 gap-y-10 mx-10 my-10 '>
+          {data.map((item) => (
+            <li key={item.Id} >
+              <h2>{item.Title}</h2>
+              <p>Price: {item.Price}</p>
+            </li>
+          ))}
+        </ul>
+        </div>
+      </div>
+      )
+    }
+  
+    return <div>Loading...</div>;
+  }
+  
 
 export default hall
--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
+var cors = require('cors');
+
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Tillu0201",
+    database:"Lingeswara",
+})
 
 app.get('/', (req, res) => {
-  res.send("Hello World")
+  res.send("Backen server")
 })
+
+
+app.get('/products', (req, res) => {
+    const q = "SELECT * FROM products"
+    db.query(q, (error, data) => {
+      if (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    } else {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+        res.setHeader('Access-Control-Allow-Methods', 'GET');
+        res.json(data);
+      }
+    });
+  })
+
+ 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
